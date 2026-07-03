8eb972e73f0227bdb6db1be35dca5b1e7b29e1c5
Israel Deku <aelisr62@gmail.com>
2026-07-03 13:14:12 +0000

    Updated project title

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>MicroFinance Banking System</title>
+    <title>Durwell Savings & Loans</title>    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/dashboard.css">
